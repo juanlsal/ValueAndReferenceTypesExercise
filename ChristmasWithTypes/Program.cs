00ecea09fdc512edc23bbe430624ec44633a5473
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-02 22:51:43 -0500

    completed

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -8,16 +8,13 @@ namespace ChristmasWithTypes
         {
             var xmas = new Christmas();
 
-            var xmasDay = Christmas.Day.Thursday;
+            var xmasDay = Christmas.Day.thursday;
 
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            xmas.Santa = "Kris Kringle";
 
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{"PS6", "GTA6", "Jeep" }; 
 
-            //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
@@ -25,10 +22,10 @@ namespace ChristmasWithTypes
 
             foreach (var present in xmas.Presents)
             {
-                Console.WriteLine($"    {present}");
+                Console.WriteLine($"{present}");
             }
 
-            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+            Console.WriteLine($"\nWe like to call Santa, {xmas.Santa}");
         }
     }
 }
